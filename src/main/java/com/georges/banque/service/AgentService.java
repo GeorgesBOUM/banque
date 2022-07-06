4b9b9e9be068147965cebbe24c63488389b0a26b
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-07-06 16:46:53 +0200

    Create AgentService.java

---
+++ src/main/java/com/georges/banque/service/AgentService.java
@@ -0,0 +1,25 @@
+package com.georges.banque.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.georges.banque.model.Agent;
+import com.georges.banque.repository.AgentRepository;
+import com.google.common.collect.Lists;
+
+@Service
+public class AgentService {
+    @Autowired
+    AgentRepository ar;
+    
+    public Optional<Agent> getAgentById(Long id) {
+	return ar.findById(id);
+    }
+    
+    public List<Agent> getAllAgents() {
+	return Lists.newArrayList(ar.findAll());
+    }
+}
