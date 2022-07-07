4da4145c693dbf0d2db5bea3a8943c06de1d0ec0
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-07-07 16:33:57 +0200

    Update AgentController.java

--- src/main/java/com/georges/banque/controller/AgentController.java
+++ src/main/java/com/georges/banque/controller/AgentController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.georges.banque.model.Agent;
+import com.georges.banque.model.Client;
 import com.georges.banque.service.AgentService;
 
 @RestController
@@ -30,8 +34,28 @@ public class AgentController {
     public Optional<Agent> getAgentByName(@PathVariable String name){
 	List<Agent> listeDesAgents = this.getAllAgents();
 	Optional<Agent> agentTrouve =  listeDesAgents.stream()
-		      .filter(agent -> agent.getNom().equals(name))
-		      .findFirst();
+		      				     .filter(agent -> agent.getNom().equals(name))
+		      				     .findFirst();
 	return agentTrouve;
+    }
+    
+    @PostMapping("/Agent")
+    public Agent addAgent(Agent agent) {
+	return as.addAgent(agent);
+    }
+    
+    @PutMapping("/Agent/{agent}/{client}")
+    public void addClientToAgent(@PathVariable Agent agent, @PathVariable Client client) {
+	agent.getListeDeClients().add(client);
+    }
+    
+    @DeleteMapping("/Agent/{agent}")
+    public void deleteAgent(@PathVariable Agent agent) {
+	as.deleteAgent(agent);
+    }
+    
+    @DeleteMapping("/Agent/{agent}/{client}")
+    public boolean deleteClient(@PathVariable Agent agent, @PathVariable Client client) {
+	return agent.getListeDeClients().remove(client);
     }
 }
